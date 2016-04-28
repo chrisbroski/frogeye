72db451005eb34d1dfa101cee6f52b0894e63e59
Chris Broski <chrisbroski@gmail.com>
2016-04-28 00:14:04 +0000

    refactoring

--- motion-overall.js
+++ motion-overall.js
@@ -1,22 +1,26 @@
+/*jslint node: true */
+
 var spawn = require('child_process').spawn,
     imgWidth = 64,
     imgHeight = 48,
     imgPixelSize = imgWidth * imgHeight,
     imgRawFileSize = imgPixelSize * 1.5,
-    timeLapseInterval = 500, // in milliseconds
     changeAmount = 50; // Value difference to be considered changed
 
 function startTimeLapseCapture() {
-    var cam = spawn('raspiyuv', [
-            '-w', imgWidth.toString(10),
-            '-h', imgHeight.toString(10),
-            '-tl', timeLapseInterval.toString(10),
-            '-t', '300000', // Restart every 5 min
-            '-o', '-' // To stdout
-        ]),
-        lastImage = [];
+    "use strict";
+    var cam, lastImage = [];
 
     console.log('Initializing time lapse image capture to stdout.');
+
+    cam = spawn('raspiyuv', [
+        '-w', imgWidth.toString(10), // image width
+        '-h', imgHeight.toString(10), // image height
+        '-tl', 0, // take photos as fast as possible
+        '-p', '50, 80, 400, 300', // small preview window
+        '-t', '300000', // Restart every 5 min
+        '-o', '-' // To stdout
+    ]);
 
     cam.stdout.on('data', function (data) {
         var thisImage = [],
@@ -37,8 +41,8 @@ function startTimeLapseCapture() {
         if (lastImage.length) {
             // Compare current and previous brightness arrays
             for (ii = 0; ii < imgPixelSize; ii += 1) {
-                diff = lastImage[ii] - thisImage[ii];
-                if (diff > changeAmount || diff < changeAmount * -1) {
+                diff = Math.abs(lastImage[ii] - thisImage[ii]);
+                if (diff > changeAmount) {
                     movement += 1;
                 }
             }
